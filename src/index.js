4837564b5d4af80eff549a1bd837cb8b07b931fa
Yangying Lin <ylin170@my.centennialcollege.ca>
2024-06-22 21:32:55 -0400

    change the password

--- src/index.js
+++ src/index.js
@@ -1,32 +1,3 @@
-/*const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const productRoutes = require('./routes/productRoutes');
-
-const app = express();
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Routes
-app.use('/api', productRoutes); // Example base path
-
-// MongoDB connection
-const mongoURI = 'mongodb+srv://ylin170:oEgThvmAtH1VOnKr@cluster0.bglp1au.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('MongoDB connection error:', err));
-
-// Server
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-}); */
-
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
