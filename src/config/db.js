1d7519c7ac14f9d70a184f70fbaccb937f530bc8
Yangying Lin <ylin170@my.centennialcollege.ca>
2024-06-22 22:50:58 -0400

    change the Link

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURI = 'mongodb+srv://ylin170:Ly50804813@cluster0.bglp1au.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+    const mongoURI = 'mongodb+srv://ylin170:Ly50804813@cluster0.bglp1au.mongodb.net/Marketplace?retryWrites=true&w=majority&appName=Cluster0';
     await mongoose.connect(mongoURI);
     console.log('MongoDB connected');
   } catch (err) {
